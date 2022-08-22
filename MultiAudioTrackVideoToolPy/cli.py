3b46c99b703bd00ee8931702df64970c6dacb0a7
aoirint <aoirint@gmail.com>
2022-08-22 12:43:58 +0900

    impl slice

--- MultiAudioTrackVideoToolPy/cli.py
+++ MultiAudioTrackVideoToolPy/cli.py
@@ -4,12 +4,27 @@ from pathlib import Path
 
 from api import config
 from api.inputs import ffmpeg_get_input
+from api.slice import ffmpeg_slice
 
 
 def command_input(args):
   input_path = Path(args.input_path)
   print(ffmpeg_get_input(
     input_path=input_path,
+  ))
+
+
+def command_slice(args):
+  ss = args.ss
+  to = args.to
+  input_path = Path(args.input_path)
+  output_path = Path(args.output_path)
+
+  print(ffmpeg_slice(
+    ss=ss,
+    to=to,
+    input_path=input_path,
+    output_path=output_path,
   ))
 
 
@@ -24,6 +39,13 @@ def main():
   parser_input = subparsers.add_parser('input')
   parser_input.add_argument('-i', '--input_path', type=str, required=True)
   parser_input.set_defaults(handler=command_input)
+
+  parser_slice = subparsers.add_parser('slice')
+  parser_slice.add_argument('-ss', type=str, required=True)
+  parser_slice.add_argument('-to', type=str, required=True)
+  parser_slice.add_argument('-i', '--input_path', type=str, required=True)
+  parser_slice.add_argument('output_path', type=str)
+  parser_slice.set_defaults(handler=command_slice)
 
   args = parser.parse_args()
 
