d1cbbe1bde63ebe4b355ac67a4a1b3bc95b60baf
aoirint <aoirint@gmail.com>
2025-09-27 00:33:39 +0900

    fix: モジュール実行時の関数を同期関数に変更 (#126)

--- aoirint_matvtool/__main__.py
+++ aoirint_matvtool/__main__.py
@@ -1,6 +1,4 @@
-import asyncio
-
 from .cli import main
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
--- binary_entrypoint.py
+++ binary_entrypoint.py
@@ -2,9 +2,7 @@
 PyInstallerでバイナリビルドするときのエントリーポイント
 """
 
-import asyncio
-
 from aoirint_matvtool.cli import main
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
